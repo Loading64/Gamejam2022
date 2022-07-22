0659ed17c5a3432fde41e34e4fe0ad686d6c3c27
Jaden Cooper (1000101880) <COOPJE1@student.op.ac.nz>
2022-07-23 11:21:28 +1200

    Made Changes To Sound Manager

--- Gamejam2022/Assets/Scripts/Managers/SoundManager.cs
+++ Gamejam2022/Assets/Scripts/Managers/SoundManager.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public static SoundManager Instance;
+    private Vector3 cameraPosition;
+    void Awake()
     {
-        
+        Instance = this;
+        cameraPosition = Camera.main.transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PlaySound(AudioClip clip)
     {
-        
+        AudioSource.PlayClipAtPoint(clip, cameraPosition);
+    }
+
+    public void PlayAttackSound(string weapon)
+    {
+        switch(weapon)
+        {
+            case "laser gun":
+                break;
+            case "laser cannon":
+                break;
+
+            default:
+                Debug.Log("Attack Sound Broke");
+                break;
+        }
+    }
+
+    public void PlayUtilitySound(string item)
+    {
+        switch(item)
+        {
+            case "Thruster":
+                break;
+            case "Shield":
+                break;
+            case "Shield Broken":
+                break;
+
+            default:
+                Debug.Log("Utility Sound Broke");
+                break;
+        }
     }
 }
