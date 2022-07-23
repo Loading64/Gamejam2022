9d86965d08207c1cebbab889d1b712b6c0521ff2
alex209175 <108102304+sudoAlex209@users.noreply.github.com>
2022-07-23 18:27:09 +1200

    Pick up hands working

--- Gamejam2022/Assets/Scripts/Hands.cs
+++ Gamejam2022/Assets/Scripts/Hands.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Hands : MonoBehaviour
 {
     public Transform[] hands;
-    private Transform[] child;
+    private Transform[] child = new Transform[2];
+    public TriggerPlayer triggerPlayer;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(hands[0]);
-        child[0] = hands[0].Find("Bone");//.transform.Find("Bone.009");
-        child[1] = hands[1].Find("Bone");//.transform.Find("Bone.009");
+        child[0] = hands[0].Find("Bone").transform.Find("Bone.009");
+        child[1] = hands[1].Find("Bone").transform.Find("Bone.009");
         child[0].gameObject.SetActive(false);
         child[1].gameObject.SetActive(false);
     }
@@ -19,6 +20,9 @@ public class Hands : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(triggerPlayer.isCollecting) {
+            child[0].gameObject.SetActive(true);
+            child[1].gameObject.SetActive(true);
+        }
     }
 }
